89f68ff816ee60d277a15d0d69812c0d7863dd5d
Aneesh Roy <aneesh.roy@gmail.com>
2022-08-04 03:59:00 -0500

    swaps

--- app.py
+++ app.py
@@ -31,7 +31,7 @@ def help():
     commands += "\n/countries/<country>/regions - (GET) All Regions associated with a given Country in the sighting data"
     commands += "\n/countries/<country>/regions/<region> - (GET) All information about a specific Region in the sighting data"
     commands += "\n/countries/<country>/regions/<region>/cities - (GET) All Cities associated with a given Country and Region in the sighting data"
-    commands += "\n/countries/<country>/regions/<region>/city - (GET) All information about a specific City in the sighting data"
+    commands += "\n/countries/<country>/regions/<region>/cities/<city> - (GET) All information about a specific City in the sighting data"
 
     return commands
 
@@ -41,6 +41,9 @@ def load_data():
     
     """
     Reads in XML data files as global dicts
+
+    Args:
+        None
 
     Returns:
         A string indicating the loading is successful.
@@ -58,9 +61,62 @@ def load_data():
     with open('XMLsightingData_citiesUSA04.xml', 'r') as sight:
         sight_data = xmltodict.parse(sight.read())
 
-    return 'Data loading is complete.\n'
+    return 'Data loaded into application.\n'
+
+@app.route('/epochs', methods=['GET'])
+def epochs():
+    """
+    Takes all of the epochs from the positional data and displays as a string
+
+    Args:
+        None
+    
+    Returns:
+        epochs: String containing all epochs
+    
+    """
+
+    logging.info("getting epochs")
+
+    epochs = ''
+
+    for i in range(len(pos_data['ndm']['oem']['body']['segment']['data']['stateVector'])):
+        epochs += (str(pos_data['ndm']['oem']['body']['segment']['data']['stateVector'][i]['EPOCH']) + '\n')
+
+    return epochs
 
 
+@app.route('/epochs/<epochs>', methods=['GET'])
+def epoch_info():
+
+    """
+    Finds the epoch given and returns all information about the specific epoch.
+
+    Args:
+        epoch: String of epoch name selected
+
+    Returns:
+        epoch_data: 
+    
+    """
+
+@app.route('/countries', methods=['GET'])
+def countries():
+
+@app.route('/countries/<countries>', methods=['GET'])
+def country_info():
+
+@app.route('/countries/<country>/regions', methods=['GET'])
+def regions():
+
+@app.route('/countries/<country>/regions/<region>', methods=['GET'])
+def region_info():
+
+@app.route('/countries/<country>/regions/<region>/cities', methods=['GET'])
+def cities():
+
+@app.route('/countries/<country>/regions/<region>/cities/<city>', methods=['GET'])
+def city_info():
 
 if __name__ == '__main__':
     app.run(debug = True, host = '0.0.0.0')
